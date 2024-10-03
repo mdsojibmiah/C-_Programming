e1816305a2eb590cc551351bdf7f9cd3065f0188
MD. SAJIB MIAH <89442408+mdsojibmiah@users.noreply.github.com>
2024-10-03 07:47:33 +0600

    Add files via upload
    variables - C++

---
+++ Variables.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int myNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (text)
+    bool myBoolean = true;       // Boolean (true or false)
+
+    cout <<"Integer Number = " << myNum << endl;
+    cout <<"Floating Number = " << myFloatNum << endl ;
+    cout <<"My Letter = " << myLetter << endl;
+    cout <<"MY text = " << myText << endl;
+    cout <<"My Boolean = "<< myBoolean << endl;
+
+    // Declare Many Variables
+    int x=10,y=20,z=30;
+    cout << "Result =  " << x+y+z << endl;
+
+
+    return 0;
+}
